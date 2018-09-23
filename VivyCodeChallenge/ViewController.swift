94e753ad62634ac29a68b7cab89dda6dd3be5e10
mailabt <maila@boostthyroid.com>
2018-09-23 13:09:01 +0200

    Initial Commit

---
+++ VivyCodeChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VivyCodeChallenge
+//
+//  Created by maila manzur on 23.09.18.
+//  Copyright © 2018 maila manzur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
